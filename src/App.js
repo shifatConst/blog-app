abda525b228303e020b2709137e9b7f1ea13d9c1
shifat <shifatreza44@gmail.com>
2021-07-06 10:28:27 +0600

    home page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './components/Home/Home';
+import UploadBlogs from './components/UploadBlogs/UploadBlogs';
 function App() {
   return (
     <div>
-      
+      <Home />
+      <UploadBlogs />
     </div>
   );
 }
---
+++ src/components/UploadBlogs/UploadBlogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UploadBlogs = () => {
+    return (
+        <div>
+            <h1>This is uploading blogs</h1>
+        </div>
+    );
+};
+
+export default UploadBlogs;
