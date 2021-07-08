32b997c994c0eec1b1634d0a594902a39087d00e
shifat <shifatreza44@gmail.com>
2021-07-08 14:19:02 +0600

    react form install and setup done

--- src/components/UploadBlogs/UploadBlogs.js
+++ src/components/UploadBlogs/UploadBlogs.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const UploadBlogs = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
+    // const handleImageUpload = () => {
+    //     console.log("This is form");
+    // }
     return (
         <div>
-            <h1>This is uploading blogs</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input defaultValue="test" {...register("example")} />
+                <input {...register("exampleRequired", { required: true })} />
+                {errors.exampleRequired && <span>This field is required</span>}
+                <input type="submit" />
+            </form>
         </div>
     );
 };
