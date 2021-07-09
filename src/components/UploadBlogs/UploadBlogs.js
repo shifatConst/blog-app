0d243ab6cd9a155e992a9914b99daf1c8251d367
shifat <shifatreza44@gmail.com>
2021-07-09 12:10:46 +0600

    image uploading successfully

--- src/components/UploadBlogs/UploadBlogs.js
+++ src/components/UploadBlogs/UploadBlogs.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 import { useForm } from "react-hook-form";
 
 const UploadBlogs = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
 
-    // const handleImageUpload = () => {
-    //     console.log("This is form");
-    // }
+    const handleImageUpload = (event) => {
+        console.log(event.target.files[0]);
+        const imageData = new FormData();
+        imageData.set('key', '763892cb50d79b3e8919930228c785b3');
+        imageData.append('image', event.target.files[0]);
+
+        axios.post('https://api.imgbb.com/1/upload', imageData)
+          .then(function (response) {
+            console.log(response.data.data.display_url);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+        
+    }
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input defaultValue="test" {...register("example")} />
-                <input {...register("exampleRequired", { required: true })} />
+                <input placeholder="Title" {...register("title", { required: true })} />
+                <br />
+                <input placeholder="Description" {...register("description", { required: true })} />
+                <br />
                 {errors.exampleRequired && <span>This field is required</span>}
+                <input type="file" onChange={handleImageUpload} />
+                <br />
                 <input type="submit" />
             </form>
         </div>
